c3ae14418f2ce5424048f33aa86b9f7e2c843fed
Atil Samancioglu <samancioglu.atil@gmail.com>
2018-09-24 22:41:05 +0300

    swift 4 & 4.2 added separetly

--- AlertProject/ViewController.swift
+++ AlertProject/ViewController.swift
@@ -22,30 +22,41 @@ class ViewController: UIViewController {
 
     @IBAction func buttonClicked(_ sender: Any) {
         
+        //swift 4
+        //let alert = UIAlertController(title: "Error", message: "Kullanıcı Adı Yanlış!", preferredStyle: UIAlertControllerStyle.alert)
+        
+        
+        //swift 4.2
+        /*
+         let alert = UIAlertController(title: "Error", message: "Kullanıcı Adı Yanlış!", preferredStyle: UIAlertController.Style.alert)
+         let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+         alert.addAction(okButton)
+         self.present(alert, animated: true, completion: nil)
+         */
+        
+        //username control
+        
         if userNameText.text == "" {
-            let alert = UIAlertController(title: "Alert", message: "Username Is Empty", preferredStyle: UIAlertController.Style.alert)
-            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+            let alert = UIAlertController(title: "Hata", message: "Kullanıcı Adı Boş!", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
             alert.addAction(okButton)
             self.present(alert, animated: true, completion: nil)
         } else if passwordText.text == "" {
-            let alert = UIAlertController(title: "Alert", message: "Password Is Empty", preferredStyle: UIAlertController.Style.alert)
-            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+            let alert = UIAlertController(title: "Hata", message: "Şifre Boş!", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
             alert.addAction(okButton)
             self.present(alert, animated: true, completion: nil)
         } else if passwordText.text != password2Text.text {
-            let alert = UIAlertController(title: "Alert", message: "Passwords Are Not Matching", preferredStyle: UIAlertController.Style.alert)
-            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+            let alert = UIAlertController(title: "Hata", message: "Şifreler Tutmuyor!", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
             alert.addAction(okButton)
             self.present(alert, animated: true, completion: nil)
         } else {
-            let alert = UIAlertController(title: "Alert", message: "User Signed Up", preferredStyle: UIAlertController.Style.alert)
-            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+            let alert = UIAlertController(title: "Hata", message: "Kayıt Edildi!", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
             alert.addAction(okButton)
             self.present(alert, animated: true, completion: nil)
         }
-        
-        
-        
         
     }
     
