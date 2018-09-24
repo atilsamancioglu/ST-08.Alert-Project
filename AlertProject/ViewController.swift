77681b5df1d35f33aabce3152410db5c56aaaeb0
Atil Samancioglu <samancioglu.atil@gmail.com>
2018-09-24 22:18:55 +0300

    swift 4.2

--- AlertProject/ViewController.swift
+++ AlertProject/ViewController.swift
@@ -23,23 +23,23 @@ class ViewController: UIViewController {
     @IBAction func buttonClicked(_ sender: Any) {
         
         if userNameText.text == "" {
-            let alert = UIAlertController(title: "Alert", message: "Username Is Empty", preferredStyle: UIAlertControllerStyle.alert)
-            let okButton = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+            let alert = UIAlertController(title: "Alert", message: "Username Is Empty", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
             alert.addAction(okButton)
             self.present(alert, animated: true, completion: nil)
         } else if passwordText.text == "" {
-            let alert = UIAlertController(title: "Alert", message: "Password Is Empty", preferredStyle: UIAlertControllerStyle.alert)
-            let okButton = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+            let alert = UIAlertController(title: "Alert", message: "Password Is Empty", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
             alert.addAction(okButton)
             self.present(alert, animated: true, completion: nil)
         } else if passwordText.text != password2Text.text {
-            let alert = UIAlertController(title: "Alert", message: "Passwords Are Not Matching", preferredStyle: UIAlertControllerStyle.alert)
-            let okButton = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+            let alert = UIAlertController(title: "Alert", message: "Passwords Are Not Matching", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
             alert.addAction(okButton)
             self.present(alert, animated: true, completion: nil)
         } else {
-            let alert = UIAlertController(title: "Alert", message: "User Signed Up", preferredStyle: UIAlertControllerStyle.alert)
-            let okButton = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+            let alert = UIAlertController(title: "Alert", message: "User Signed Up", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
             alert.addAction(okButton)
             self.present(alert, animated: true, completion: nil)
         }
